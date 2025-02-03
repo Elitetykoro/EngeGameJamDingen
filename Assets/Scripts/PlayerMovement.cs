29d60b3c35e1febdeea3eab2cce0cc6b0b714b8d
Elitetykoro <tykoromathijssen@gmail.com>
2025-02-03 11:24:21 +0100

    PlayerMovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 playerPosition;
+    [SerializeField] float moveSpeed;
+    void Update()
+    {
+        playerPosition += new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0, 0) * Time.deltaTime;
+        playerPosition += new Vector3(0, Input.GetAxis("Vertical") * moveSpeed, 0) * Time.deltaTime;
+
+        transform.position = playerPosition;
+    }
+}
