cc6abd28c20f5a71238c2279ca97783a9771ef4f
julien oving <144676946+isheeSAURUS2@users.noreply.github.com>
2025-02-04 15:53:45 +0100

    gyaaaaaaat mega fix

--- Assets/Scripts/Player/PlayerHit.cs
+++ Assets/Scripts/Player/PlayerHit.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerHit : MonoBehaviour
 {
     public PlayerState currentPlayerState;
-    BoxCollider2D playerCollision;
+    
     SpriteRenderer playerRenderer;
     float timer = 0;
     [SerializeField] float invincibleDuration;
@@ -17,7 +17,7 @@ public class PlayerHit : MonoBehaviour
     }
     void Update()
     {
-        playerCollision = GetComponent<BoxCollider2D>();
+       
         playerRenderer = GetComponent<SpriteRenderer>();
 
         if(timer > invincibleDuration)
@@ -30,25 +30,25 @@ public class PlayerHit : MonoBehaviour
         {
             case PlayerState.Invincible:
                     timer += Time.deltaTime;
-                    playerCollision.enabled = false;
+                    
                     playerRenderer.color = new Color(playerRenderer.color.r, playerRenderer.color.g, playerRenderer.color.b, playerRenderer.color.a - (Time.deltaTime * 5));
                     if (playerRenderer.color.a <= 0) playerRenderer.color = new Color(playerRenderer.color.r, playerRenderer.color.g, playerRenderer.color.b, 1);
                     break;
             case PlayerState.Dash:
                 timer += Time.deltaTime;
-                playerCollision.enabled = false;
+                
                 //playerRenderer.color = new Color(playerRenderer.color.r, playerRenderer.color.g, playerRenderer.color.b, playerRenderer.color.a - (Time.deltaTime * 5));
                 //if (playerRenderer.color.a <= 0) playerRenderer.color = new Color(playerRenderer.color.r, playerRenderer.color.g, playerRenderer.color.b, 1);
                 break;
             case PlayerState.Hitable:
-                playerCollision.enabled = true;
+                
                 playerRenderer.color = new Color(1,1,1,1);
                 break;
         }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Bullet")
+        if (other.gameObject.tag == "Bullet"&&currentPlayerState != PlayerState.Invincible&&currentPlayerState != PlayerState.Dash)
         {
             currentPlayerState = PlayerState.Invincible;
             playSoundEffect();
